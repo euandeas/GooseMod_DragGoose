02fda5d8668a4ee8acd639c8dcf08829ee87d2ab
Euan Deas <euandeas@hotmail.com>
2020-02-13 11:37:00 +0000

    Add project files.

---
+++ DefaultMod/ModMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 1. Added the "GooseModdingAPI" project as a reference.
+// 2. Compile this.
+// 3. Create a folder with this DLL in the root, and *no GooseModdingAPI DLL*
+using GooseShared;
+using SamEngine;
+
+namespace DragGoose
+{
+    public class ModEntryPoint : IMod
+    {
+
+        Random rnd = new Random();
+        // Gets called automatically, passes in a class that contains pointers to
+        // useful functions we need to interface with the goose.
+        void IMod.Init()
+        {
+            // Subscribe to whatever events we want
+            InjectionPoints.PostTickEvent += PostTick;
+        }
+
+        public void PostTick(GooseEntity g)
+        {
+            if (IsMouseOnGoose(g) && Input.leftMouseButton.Held == true && g.currentTask != 4)
+            {
+                g.position = new Vector2(Input.mouseX-5, Input.mouseY);
+                g.direction = g.direction+rnd.Next(-10, 10);
+            }
+
+        }
+
+        public bool IsMouseOnGoose(GooseEntity g)
+        {
+            float xDif = g.position.x - Input.mouseX;
+            float yDif = g.position.y - Input.mouseY;
+
+            if (((-40 < xDif) && (xDif < 40)) && ((-40 < yDif) && (yDif < 40)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
